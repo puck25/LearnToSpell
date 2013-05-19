6d21d560f3b56ae34d2aeb2a5269b51921dc6e72
Keir Frampton <Keirframpton@gmail.com>
2013-05-19 22:26:27 +0100

    still working on FE redesign - adding logic

--- answers.js
+++ answers.js
@@ -4,89 +4,89 @@
     [
         { "Name": "Level1", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level2", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level3", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level4", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },        { "Name": "Level5", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level6", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level7", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level8", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level9", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
         },
                 { "Name": "Level10", "Answers":
             [
-                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="148" width="148">' },
-                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="148" width="148">' }
+                { "Word": "cat", "imgref": '<img class="mainImages" src="/imganswers/cat.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "dog", "imgref": '<img class="mainImages" src="/imganswers/dog.jpg" alt="Cat" height="100" width="100">' },
+                { "Word": "rat", "imgref": '<img class="mainImages" src="/imganswers/rat.jpg" alt="Cat" height="100" width="100">' }
 
             ]
 
--- gameLogic.js
+++ gameLogic.js
@@ -18,9 +18,12 @@
     //hide instruction screen
     $(document).ready(function () {
         $('#instructionsButton').click(function () {
+            $('#cloudimgdiv').show('puff', 1000);
+            $('#stats').animate({ left: '+=295' }, 1000);
             $('#instructions').slideUp(600, function () {
-                $('#result').fadeIn(1000, function () {
-                    $('#result').html('Click play to begin!');
+                $('#finish').fadeIn(1000, function () {
+                    $('#finish').html('Click play to begin!');
+
                 });
             });
         });
@@ -29,7 +32,7 @@
     //code the loads the play command
     $(document).ready(function () {
         $('#play').click(function () {
-            $('#result').fadeOut(1000);
+            $('#finish').fadeOut(1000);
             $('span').html(' ' + level);
             $('#imgLoad').html(levels[currentLevel].Answers[currentAnswer].imgref);
             $('#progressbar').fadeIn('slow');
@@ -60,11 +63,10 @@
             if (score === currentlevelLenghtt) {
                 animate(progress);
                 $('.mainImages').effect("explode", 500);
-                $('#imgLoad').html(pic);
+                $('#starinc').html(starpoints);
                 $('.mainImages').fadeIn(1000);
                 $('#points').html(score);
-                $('#result').html('Well done, you have completed level!' + ' ' + Levelcomplete + '.' + 'Here is a gold star!');
-                $('#starpoints').html(starpoints);
+                $('#finish').html('Well done, you have completed level!' + ' ' + Levelcomplete + '.' + 'Here is a gold star!');
                 $('#imgload').effect("bounce", 1000, function () {
                     $('#imgLoad').html(star.image);
                 });
@@ -77,9 +79,9 @@
                 var pic = levels[currentLevel].Answers[currentAnswer].imgref;
                 $('input[name=Word]').val('');
                 $('.mainImages').effect("explode", 500);
-                $('#result').fadeIn(1000);
-                $('#result').html("WOW! You are right, great work!");
-                $('#points').html(score);
+                $('#finish').fadeIn(1000);
+                $('#finish').html("WOW! You are right, great work!");
+                $('#pointsinc').html(score);
                 animate(progress);
                 $('#imgLoad').html(pic);
                 $('.mainImages').fadeIn(1000);
@@ -87,8 +89,8 @@
 
         }
         else {
-            $('#result').html("Oops, try again!");
-            $('#result').fadeOut(3000);
+            $('#finish').html("Oops, try again!");
+            $('#finish').fadeOut(3000);
         }
     };
 
@@ -106,7 +108,7 @@
     //function that loads next level
     var nextLevel = function () { 
         $('#nextlevel').click(function(){
-            $('#result').fadeOut(1000);
+            $('#finish').fadeOut(1000);
             $('#star').fadeOut(1000);
             currentlevel++;
             $('span').html(' ' + level);
