4b8c7b500d1e2742265f5222526f71191fb35c74
puck25 <keirframpton@gmail.com>
2013-06-12 18:11:30 +0100

    added enter key down event for submitting answers

--- gameLogic.js
+++ gameLogic.js
@@ -1,133 +1,133 @@
+
+//loads a gold star image when ever called
+var star = {image:'<img class="mainImages" src="/images/star.png" alt="Cat" height="100" width="100">'}
+
+//declare all varilables
+var currentLevel = 0;
+var currentAnswer = 0;
+var currentImage = 0;
+var level = 1;
+var score = 0;
+var starpoints = 1;
+var currentlevelLenght = levels[currentLevel].Answers.length;
+var totalAmountofLevels = levels.lenght;
+var Levelcomplete = 1;
+var lastScore = 0;
+
+
 $(document).ready(function () {
     $('#instructions').draggable({ appendTo: "#instructions" });
-});
-    
-    //loads a gold star image when ever called
-    var star = {image:'<img class="mainImages" src="/images/star.png" alt="Cat" height="100" width="100">'}
-
-    //declare all varilables
-    var currentLevel = 0;
-    var currentAnswer = 0;
-    var currentImage = 0;
-    var level = 1;
-    var score = 0;
-    var starpoints = 1;
-    var currentlevelLenght = levels[currentLevel].Answers.length;
-    var totalAmountofLevels = levels.lenght;
-    var Levelcomplete = 1;
-    var lastScore = 0;
-
 
     //hide instruction screen
-    $(document).ready(function () {
-        $('#instructionsButton').click(function () {
-            $('#cloudimgdiv').show('puff', 1000);
-            $('#stats').animate({ left: '+=295' }, 1000);
-            $('#instructions').slideUp(600, function () {
-                $('#finish').fadeIn(1000, function () {
-                    $('#finish').html('Click play to begin!');
+    $('#instructionsButton').click(function () {
+        $('#cloudimgdiv').show('puff', 1000);
+        $('#stats').animate({ left: '+=295' }, 1000);
+        $('#instructions').slideUp(600, function () {
+            $('#finish').fadeIn(1000, function () {
+                $('#finish').html('Click play to begin!');
 
-                });
             });
         });
     });
 
     //code the loads the play command
-    $(document).ready(function () {
-        $('#play').click(function () {
-            $('#finish').fadeOut(1000);
-            $('span').html(' ' + level);
-            $('#imgLoad').html(levels[currentLevel].Answers[currentAnswer].imgref);
-            $('#progressbar').fadeIn('slow');
-            $('#imgLoad').fadeIn('slow');
-            $('form').fadeIn('slow');
-            $('span').fadeIn('slow');
-            $('#lightning').fadeIn('slow');
-            $('#points').html(score);
+    $('#play').click(function () {
+        $('#finish').fadeOut(1000);
+        $('span').html(' ' + level);
+        $('#imgLoad').html(levels[currentLevel].Answers[currentAnswer].imgref);
+        $('#progressbar').fadeIn('slow');
+        $('#imgLoad').fadeIn('slow');
+        $('#formcontainer').fadeIn('slow');
+        $('span').fadeIn('slow');
+        $('#lightning').fadeIn('slow');
+        $('#points').html(score);
+        $('#textbox').focus();
+    });
+
+    //code that calls the main game passing in the form value
+    $('#clickme').click(function () {
+        var answer = $('input[name=Word]').val();
+        learToSpellGame(answer);
+    });
+    
+    //Allows return key to be used for answering question
+    $('#textbox').keydown(function (e) {
+        var code = e.which;
+        if (code == 13) {
+            $('#clickme').click();
+        }
+    });
+
+    //loads next level
+    $('#nextlevel').click(function () {
+        var progresslength = $('#progressbar').width();
+        $('#textbox').focus();
+        currentLevel++
+        currentAnswer = 0;
+        level++;
+        $('#finish').fadeOut(1000);
+        $('span').html(' ' + level);
+        $('input[name=Word]').val('');
+        $('#finish').fadeIn(1000);
+        $('#imgLoad').html(levels[currentLevel].Answers[currentAnswer].imgref);
+        currentlevelLenght = levels[currentLevel].Answers.length;
+        $('#lightningimg').animate({ left: '-=' + progresslength });
+        learToSpellGame();
+    });
+});
+
+
+//Main game funcational code
+var learToSpellGame = function (answer) {
+
+    //If statement that checks if the game is complete... i.e. correctAnswer is more then available.
+    var correctAnswer = levels[currentLevel].Answers[currentAnswer].Word;
+    if (answer === correctAnswer) {
+        currentAnswer++;
+        score++;
+        if (score === currentlevelLenght + lastScore) {
+            animate(progress);
             $('#textbox').focus();
-        });
-    });
-
-   
-//code that calls the main game passing in the form value
-    $(document).ready(function () {
-        $('#clickme').click(function () {
-            var answer = $('input[name=Word]').val();
-            learToSpellGame(answer);
-        });
-    });
-
-     //Main game funcational code
-    var learToSpellGame = function (answer) {
-
-        //If statement that checks if the game is complete... i.e. correctAnswer is more then available.
-        var correctAnswer = levels[currentLevel].Answers[currentAnswer].Word;
-        if (answer === correctAnswer) {
-            currentAnswer++;
-            score++;
-            if (score === currentlevelLenght + lastScore) {
-                animate(progress);
-                $('#textbox').focus();
-                $('input[name=Word]').val('');
-                $('#pointsinc').html(score);
-                $('.mainImages').effect("explode", 500);
-                $('#starsinc').html(starpoints);
-                $('.mainImages').fadeIn(1000);
-                $('#finish').html('Well done, you have completed level' + ' ' + Levelcomplete + '.' + 'Here is a gold star!');
-                $('#imgLoad').html(star.image);
-                $('.mainImages').fadeIn(1000);
-                starpoints++;
-                lastScore = score;
-                Levelcomplete++;
-
-            }
-            else {
-                var pic = levels[currentLevel].Answers[currentAnswer].imgref;
-                $('#textbox').focus();
-                $('input[name=Word]').val('');
-                $('.mainImages').effect("explode", 500);
-                $('#finish').fadeIn(1000);
-                $('#finish').html("WOW! You are right, great work!");
-                $('#pointsinc').html(score);
-                animate(progress);
-                $('#imgLoad').html(pic);
-                $('.mainImages').fadeIn(1000);
-            };
+            $('input[name=Word]').val('');
+            $('#pointsinc').html(score);
+            $('.mainImages').effect("explode", 500);
+            $('#starsinc').html(starpoints);
+            $('.mainImages').fadeIn(1000);
+            $('#finish').html('Well done, you have completed level' + ' ' + Levelcomplete + '.' + 'Here is a gold star!');
+            $('#imgLoad').html(star.image);
+            $('.mainImages').fadeIn(1000);
+            starpoints++;
+            lastScore = score;
+            Levelcomplete++;
 
         }
         else {
-            $('#finish').html("Oops, try again!");
-            $('#finish').fadeOut(3000);
-        }
-    };
-
-     //works out the length of the progressbar and divs the value by the amount of questions in the level
-    var progress = function () {
-        var progresslength = $('#progressbar').width();
-        return progresslength / currentlevelLenght;
-    };
-
-    //animates the progress icon to show progress of the level.
-    var animate = function (progress) {
-        $('#lightningimg').animate({ left: '+=' + progress() });
-    };
-
-
-        //loads next level
-    $(document).ready(function () {
-        $('#nextlevel').click(function () {
-            var progresslength = $('#progressbar').width();
+            var pic = levels[currentLevel].Answers[currentAnswer].imgref;
             $('#textbox').focus();
-            currentLevel++
-            currentAnswer = 0;
-            level++;
-            $('#finish').fadeOut(1000);
-            $('span').html(' ' + level);
             $('input[name=Word]').val('');
+            $('.mainImages').effect("explode", 500);
             $('#finish').fadeIn(1000);
-            $('#imgLoad').html(levels[currentLevel].Answers[currentAnswer].imgref);
-            currentlevelLenght = levels[currentLevel].Answers.length;
-            $('#lightningimg').animate({ left: '-=' + progresslength});
-            learToSpellGame();
-        });
-    });
+            $('#finish').html("WOW! You are right, great work!");
+            $('#pointsinc').html(score);
+            animate(progress);
+            $('#imgLoad').html(pic);
+            $('.mainImages').fadeIn(1000);
+        };
+
+    }
+    else {
+        $('#finish').html("Oops, try again!");
+        $('#finish').fadeOut(3000);
+    }
+};
+
+//works out the length of the progressbar and divs the value by the amount of questions in the level
+var progress = function () {
+    var progresslength = $('#progressbar').width();
+    return progresslength / currentlevelLenght;
+};
+
+//animates the progress icon to show progress of the level.
+var animate = function (progress) {
+    $('#lightningimg').animate({ left: '+=' + progress() });
+};
